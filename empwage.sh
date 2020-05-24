131289741c5ae1e7ad3b36afe0738fd8cdd9da88
suyog-6996 <gabhane.suyog@gmail.com>
2020-05-24 23:57:08 +0530

    ADDED:parttime wage calc

--- empwage.sh
+++ empwage.sh
@@ -1,13 +1,18 @@
 #!/bin/bash -x
 
-isPresent=1;
-randomCheck=$(( RANDOM%2 ));
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+randomCheck=$((RANDOM%3 ))
 
-if [ $isPresent -eq $randomCheck ];
+if [ $isFullTime -eq $randomCheck ]
 then
-   empRatePerHr=20;
-   empHrs=8;
-   salary=$(( $empHrs * $empRatePerHr ));
+	empHrs=8;
+elif [ $isPartTime -eq $randomCheck ]
+then
+	empHrs=4
 else
-   salary=0;
+	empHrs=0
 fi
+
+salary=$(($empHrs*$empRatePerHr ))
