6bdc27e1c9028efca67c7cbe24c93171da81d2ce
suyog-6996 <gabhane.suyog@gmail.com>
2020-05-25 01:28:34 +0530

    ADDED:Function addition

--- empwage.sh
+++ empwage.sh
@@ -1,19 +1,16 @@
-#!/bin/bash -x
+#!/bash/bin -x
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=4;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
-IS_PART_TIME=1
-IS_FULL_TIME=2
-MAX_HRS_IN_MONTH=10
-EMP_RATE_PER_HR=20
-NUM_WORKING_DAYS=20
+totalEmpHr=0;
+totalWorkingDays=0;
 
-totalEmpHr=0
-totalWorkingDays=0
-
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
-do
-	((totalWorkingDays++))
-   empCheck=$((RANDOM%3))
-      case $empCheck in
+function	getWorkHrs()
+{
+   case $1 in
          $IS_FULL_TIME)
                   empHrs=8
                   ;;
@@ -24,9 +21,17 @@ do
          empHrs=0
                   ;;
       esac
-		totalEmpHrs=$(($totalEmpHrs+$empHrs))
+      echo $empHrs
+}
 
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+   ((totalWorkingDays++))
+   empHrs="$( getWorkHrs $((RANDOM%3)) )"
+   totalEmpHrs=$((totalEmpHrs+empHrs))
 done
 
-totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
-echo $totalEmpHrs $totalSalary
+totalSalary=$((totalEmpHrs*EMP_RATE_PER_HR));
+
+#echo $totalSalary
+
