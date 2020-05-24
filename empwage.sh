75533a41aef4f10764fba2bb7b7c89b9a9192cc9
suyog-6996 <gabhane.suyog@gmail.com>
2020-05-25 01:22:55 +0530

    ADDED:Condition updated

--- empwage.sh
+++ empwage.sh
@@ -1,28 +1,32 @@
 #!/bin/bash -x
-isPartTime=1;
-isFullTime=2;
-totalSalary=0;
-empRatePerHr=20;
-numWorkingDays=20;
 
-for (( day=1; day<=$numWorkingDays; day++ ))
+IS_PART_TIME=1
+IS_FULL_TIME=2
+MAX_HRS_IN_MONTH=10
+EMP_RATE_PER_HR=20
+NUM_WORKING_DAYS=20
+
+totalEmpHr=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-	empCheck=$((RANDOM%3))
-		case $empCheck in
-			$isFullTime)
-						empHrs=8
-						;;
-			$isPartTime)
-						empHrs=4
-						;;
-			*)
-			empHrs=0
-						;;
-		esac
-
-		salary=$(($empHrs*$empRatePerHr ));
-		totalSalary=$(($totalSalary+$salary ))
+	((totalWorkingDays++))
+   empCheck=$((RANDOM%3))
+      case $empCheck in
+         $IS_FULL_TIME)
+                  empHrs=8
+                  ;;
+         $IS_PART_TIME)
+                  empHrs=4
+                  ;;
+         *)
+         empHrs=0
+                  ;;
+      esac
+		totalEmpHrs=$(($totalEmpHrs+$empHrs))
 
 done
 
-#echo $salary $totalSalary
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
+echo $totalEmpHrs $totalSalary
