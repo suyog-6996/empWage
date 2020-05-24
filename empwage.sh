8d00a367869b606307fce0c0a1ff2a48e390e7dc
suyog-6996 <gabhane.suyog@gmail.com>
2020-05-24 23:44:18 +0530

    ADDED:dailyempwage

--- empwage.sh
+++ empwage.sh
@@ -1,11 +1,13 @@
-#/bin/bash -x
+#!/bin/bash -x
 
-isPresent=1
+isPresent=1;
 randomCheck=$(( RANDOM%2 ));
 
 if [ $isPresent -eq $randomCheck ];
 then
-   echo  " Employee is present"
+   empRatePerHr=20;
+   empHrs=8;
+   salary=$(( $empHrs * $empRatePerHr ));
 else
-   echo "Employee is Abscent"
+   salary=0;
 fi
